5f6ac700228bac3e4c68eb2f95f1c721bdbcedb8
Cameron Gallarno <cgallarno@gmail.com>
2013-05-27 19:25:56 -0400

    added childbrowser for google+ sign on

--- system/setup/init.js
+++ system/setup/init.js
@@ -28,6 +28,7 @@ var $app; //inilize the $app variable at a global level
 loadCss ("system/vendor/angular-mobile-nav/mobile-nav.css");
 head.js(
 	"phonegap.js",
+	"childbrowser.js",
 	"system/vendor/jquery/jquery-2.0.0.min.js",
 	"system/vendor/EightMedia/hammer-1.0.5.min.js",
 	"system/vendor/underscore/underscore.js",
