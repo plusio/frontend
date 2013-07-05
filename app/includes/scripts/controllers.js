f0a17dfe0d2f6bbabf796a4e514e3c48d6d1d782
Andy Nunez <nuneza73@hotmail.com>
2013-07-05 19:32:26 -0400

    cleaning up comments, set 1

--- app/includes/scripts/controllers.js
+++ app/includes/scripts/controllers.js
@@ -1,17 +1,8 @@
 'use strict';
 
-/* These controller functions are setup in such a way that there is 1 controller per screen/page. 
- * Their purpose is to contain all the logic for the page such as: 
- * 1) Getting, setting and saving data.
- * 2) What happens on pages events like: button clicks, dropdown selections, etc.
- *   However all visual effects, DOM manipulations, and plugins should be done using angularjs directives.
- *   If you're not sure as to what is going on in most of this file you should consider reading this articles:
- *   1) http://blog.artlogic.com/2013/03/06/angularjs-for-jquery-developers/ 
- *   2) http://www.yearofmoo.com/2012/08/use-angularjs-to-power-your-web-application.html#controllers-and-scope
-
-
 /* Variables:
- * 1) $scope: Here we pass in the $scope dependency because this controller needs the two-way databinding functionality of angular.
+ * 1) $scope: use this variable to set data in the user interface.
+ * 2) plus: use this for all Plus.io provided functionality.
  */
 $app.controller('HomeCrtl', function ($scope, plus) {
   // defaulting the time on Angular's model variable.
--- app/themes/twentythirteen/js/theme_controllers.js
+++ app/themes/twentythirteen/js/theme_controllers.js
@@ -1,16 +1,10 @@
 'use strict';
-/*
- * In this theme controllers file, we can create controllers as we wold normally but the view has a very specific purpose and the developer 
- * won't need to code anything for this view so we can have custom functionality packaged with the theme.
- */
-
 
 /* Variables:
  * 1) $scope: Here we pass in the $scope dependency because this controller needs the two-way databinding functionality of angular.
  * 2) geolocation: The geo location service will get and return the user's current location
  * 3) $http: This is angular service to  post and get data from external sources
  */
-
 $app.controller('mapController', function($scope, geolocation, $http){
 
   //set Map defaults
@@ -23,8 +17,7 @@ $app.controller('mapController', function($scope, geolocation, $http){
     markers : {}
   };
 
-  // function to update Leaflet model once we have gotten the user's location.
-  // This is assigned to a fuction so that we can change the message if we cannot reverse geocode the Lat/Lng
+  // Update Leaflet model once we have gotten the user's location & lets us change the message if we cannot reverse geocode the Lat/Lng
   function updateData(latlng, message){
     var marker = {
       currentLocation : {
@@ -60,8 +53,6 @@ $app.controller('mapController', function($scope, geolocation, $http){
 
     //use the latlng with google maps geocode api to find the nearest address
     $http.get(sprintf('http://maps.googleapis.com/maps/api/geocode/json?latlng=%(lat)s,%(lng)s&sensor=true', latlng)).success(function(data){
-      
-      //get the formatted address
       var address = data.results[0].formatted_address;
       if(_.isEmpty(address)){
         updateData(latlng, 'You are here');
