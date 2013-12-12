291223f91d921a4ccd31b94558503d46879bbe25
Cameron Gallarno <cgallarno@gmail.com>
2013-12-12 15:45:27 -0500

    updated angular

--- plugins/angular/plugin.js
+++ plugins/angular/plugin.js
@@ -1,5 +1,18 @@
 PluginConfig({
 	name : 'Angular',
-	version : '1.0.8',
-	files : ['angularJS-1.0.8.min.js']
+	version : '1.2.4',
+	files : [
+		'angular.js',
+		'angular-animate.js',
+		'angular-cookies.js',
+		'angular-csp.css',
+		'angular-loader.js',
+		'angular-mocks.js',
+		'angular-resource.js',
+		'angular-route.js',
+		'angular-sanitize.js',
+		'angular-scenario.js',
+		'angular-touch.js',
+	],
+	angularMod : ['ngAnimate', 'ngCookies', 'ngTouch', /*'ngMock', 'mock.animate', 'ngMockE2E',*/ 'ngResource', 'ngRoute', 'ngSanitize', 'ngLocale', 'ngTouch']
 });
